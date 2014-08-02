3f1d97c993ca59af7d5ab346d6a1a184457b1c74
Joshua Jackson <jjackson@axcient.com>
2014-08-02 12:29:04 -0700

    Change to json config file and push data to graphite with pickle format

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ graphite_scripts
 
 Miscellaneous scripts to interact with various devices and feed the resulting information into graphite for monitoring/trending purposes.
 
-Will only work for the nest thermostat as I do not have information available for the smoke/co detector available currently
+Settings.cfg should go into the users home folder in their .config/graphite/ directory. Settings are True/False bool's expect serial where its false or a single serial number to query against.
+
+All data is pushed to graphite in a single pickle push to port 2004(default) to reduce the load on the server for metrics added to the environment. The script pulls from both the nest api as well as the provided nest weather api to give both indoor and outdoor temperature, humidity and other information that looks interesting.
--- graphite_nest.py
+++ graphite_nest.py
@@ -7,7 +7,12 @@
 import urllib2
 import urllib
 import sys
-#import pprint
+import pickle
+import socket
+import time
+import struct
+import yaml
+import os
 from optparse import OptionParser
 
 try:
@@ -16,13 +21,20 @@ except ImportError:
 	print "No json library installed please install python-json"
 	sys.exit(1)
 
+def temperature(value):
+	fahrenheit = (value * 1.8) + 32
+	return fahrenheit
+
+def miles(value):
+	mph = (value * .621371)
+	return mph
+
 class Nest:
-	def __init__(self, user, password, serial=None, metric=False, device=None, name=False):
+	def __init__(self, user, password, serial=None, metric=False, name=False):
 		self.username = user
 		self.password = password
 		self.serial = serial
 		self.metric = metric
-		self.device = device
 		self.name = name
 	
 	def nest_auth(self):
@@ -40,65 +52,102 @@ class Nest:
 		self.userid = res["userid"]
 		#weather api
 		self.weather_url = res["urls"]["weather_url"]
+
 	def nest_structure(self):
 		req = urllib2.Request(self.transport_url + "/v2/mobile/user." + self.userid, headers={"user-agent":"graphite_nest v/1.0", "Authorization":"Basic " + self.access_token,  "X-nl-user-id": self.userid, "X-nl-protocol-version": "1"})
 		res = urllib2.urlopen(req).read()
 		rep = json.loads(res)
 		#save reply to work from for all future requests in other functions
 		self.repsave = rep
-		#pprint.pprint(rep)
 		self.structure_id = rep["structure"].keys()[0]
 		#Grab zipcode to use with weather_url api
 		self.zipcode = rep["structure"][self.structure_id]["postal_code"]
-		self.nest_devices()
+		result = self.nest_devices()
+		return(result)
 
 	def nest_devices(self):
-		if (self.device is None):
+		self.device_list = []
+		if (self.serial is False):
 			self.device_id = self.repsave["structure"][self.structure_id]["devices"]
-			self.device_list = []
 			#devices are a combo of device.serial
 			for device in self.device_id:
 				self.device_list.append(device.split(".")[1])
 		else:
-			self.device_list = self.device
-		self.nest_serial()
+			self.device_list.append(self.serial)
+		result = self.nest_serial()
+		return(result)
 
 	def nest_serial(self):
+		weather = self.nest_weather()
 		for device in self.device_list:
-			self.device = []
+			self.device = {}
 			#pull all potentially interesting values
-			self.device.append(self.repsave["device"][device]["battery_level"])
-			self.device.append(self.repsave["device"][device]["current_humidity"])
-			self.device.append(self.repsave["device"][device]["learning_days_completed_cool"])
-			self.device.append(self.repsave["device"][device]["learning_days_completed_heat"])
-			self.device.append(self.repsave["device"][device]["learning_days_completed_range"])
-			self.device.append(self.repsave["device"][device]["learning_time"])
-			self.device.append(self.repsave["device"][device]["time_to_target"])
-			self.device.append(self.repsave["shared"][device]["current_temperature"])
+			self.device["battery_level"] = self.repsave["device"][device]["battery_level"]
+			self.device["interior_humidity"] =self.repsave["device"][device]["current_humidity"]
+			self.device["learning_days_completed_cool"] = self.repsave["device"][device]["learning_days_completed_cool"]
+			self.device["learning_days_completed_heat"] = self.repsave["device"][device]["learning_days_completed_heat"]
+			self.device["learning_days_completed_range"] = self.repsave["device"][device]["learning_days_completed_range"]
+			self.device["learning_time"] = self.repsave["device"][device]["learning_time"]
+			self.device["time_to_target_temp"] = self.repsave["device"][device]["time_to_target"]
+			if (self.metric is True):
+				self.device["current_temperature"] = self.repsave["shared"][device]["current_temperature"]
+			else:
+				self.device["current_temperature"] = temperature(self.repsave["shared"][device]["current_temperature"])
 			if (self.name is True):
-				self.device.append(self.repsave["shared"][device]["name"])
-		print self.device
+				self.device["name"] = self.repsave["shared"][device]["name"]
+			else:
+				self.device["name"] = device 
+			self.device.update(weather)
+		return(self.device)
 
-def options():
-	parser = OptionParser()
-	parser.add_option("-u", "--user", dest="user", help="username for home.nest.com", metavar="USER", default=None)
-	parser.add_option("-p", "--password", dest="password", help="password for home.nest.com", metavar="PASSWORD", default=None)
-	parser.add_option("-s", "--serial", dest="serial", default=None, help="Optional: specific nest device to talk to")
-	parser.add_option("-m", "--metric", dest="metric", action="store_true", help="Optional: switch to metric measurements")
-	parser.add_option("-d", "--device", dest="device", default=None, help="Optional: particular device to poll")
-	parser.add_option("-c", "--commonname", dest="name", action="store_true", help="Optional: Use name set for device instead of serial")
-	return parser
+	def nest_weather(self):
+		self.weather = {}
+		req = urllib2.Request(self.weather_url + self.zipcode)
+		res = urllib2.urlopen(req).read()
+		rep = json.loads(res)
+		self.weather["temperature"] = rep[self.zipcode]["current"]["temp_c"]
+		if (self.metric is True):
+			self.weather["windspeed"] = rep[self.zipcode]["current"]["wind_kph"]
+		else:
+			self.weather["windspeed"] = miles(rep[self.zipcode]["current"]["wind_kph"])
+		self.weather["exterior_humidity"] = rep[self.zipcode]["current"]["humidity"]
+		self.weather["lengthofday"] = rep[self.zipcode]["current"]["lengthofday"]
+		return(self.weather)
 
+class Graphite:
+	def __init__(self, dictionary, prefix="home.", server="localhost", port=2004):
+		self.prefix = prefix
+		self.server = server
+		self.port = int(port)
+		self.data = dictionary
+	
+	def pickling(self):
+		pickled = ([])
+		epoch = int(time.time())	
+		for key,value in self.data.iteritems():
+			pickled.append(( self.prefix + "." + self.data["name"] + "." + key, ( epoch, value)))
+		payload = pickle.dumps(pickled)
+		header = struct.pack("!L", len(payload))
+		message = header + payload
+		self.send_data(message)
+
+	def send_data(self, message):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((self.server, self.port))
+		try:
+			s.sendall(message)
+		except socket.error, (value,message):
+			print "could not send message:", message
+			sys.exit(1)
+		s.close()
 
 def main():
-	arguments = options()
-	(opts, args) = arguments.parse_args()
-	if (not opts.user) or (not opts.password):
-		print "Username and Password required for home.nest.com api integration"
-		sys.exit(1)
-	napi = Nest(opts.user, opts.password, opts.serial, opts.metric, opts.device, opts.name)
+	config = yaml.load(open(os.path.expanduser("~") + "/.config/" + "graphite/" + "settings.cfg","r").read())
+	napi = Nest(config["user"], config["password"], config["serial"], config["metric"], config["name"])
 	napi.nest_auth()
-	napi.nest_structure()
+	data = napi.nest_structure()
+	graphite = Graphite(data, config["graphite_prefix"], config["graphite_host"],config["graphite_port"])
+	graphite.pickling()
 
 if __name__=="__main__":
 	main()	
